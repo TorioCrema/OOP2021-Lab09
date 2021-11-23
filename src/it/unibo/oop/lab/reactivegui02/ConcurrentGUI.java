5a72c308fa9ddb839ff938e91ddda9520ed286c1
Simone Zama <simone.zama@studio.unibo.it>
2021-11-23 16:47:07 +0100

    Remove unused import

--- src/it/unibo/oop/lab/reactivegui02/ConcurrentGUI.java
+++ src/it/unibo/oop/lab/reactivegui02/ConcurrentGUI.java
@@ -6,7 +6,6 @@ import javax.swing.JFrame;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.lang.reflect.InvocationTargetException;
 
 import javax.swing.JLabel;
 import javax.swing.JPanel;
