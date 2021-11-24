23c7cb3c80fc688c7e8b20fd2f664fa32299aeb4
Simone Zama <simone.zama@studio.unibo.it>
2021-11-24 10:13:29 +0100

    Implement sum method

--- src/it/unibo/oop/lab/workers02/MultiThreadedSumMatrix.java
+++ src/it/unibo/oop/lab/workers02/MultiThreadedSumMatrix.java
@@ -1,6 +1,8 @@
 package it.unibo.oop.lab.workers02;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 /**
  * 
@@ -61,8 +63,28 @@ public class MultiThreadedSumMatrix implements SumMatrix {
 
     @Override
     public double sum(final double[][] matrix) {
-        // TODO Auto-generated method stub
-        return 0;
+        final int size = matrix.length % numThreads + matrix.length / numThreads;
+        // Build a new list of workers
+        final List<Worker> workers = new ArrayList<>(numThreads);
+        for (int start = 0; start < matrix.length; start += size) {
+            workers.add(new Worker(matrix, start, size));
+        }
+        // Start them
+        for (final Worker w : workers) {
+            w.start();
+        }
+        // Wait for every one of them to finish
+        double sum = 0;
+        for (final Worker w : workers) {
+            try {
+                w.join();
+                sum += w.getResult();
+            } catch (InterruptedException e) {
+                throw new IllegalStateException(e);
+            }
+        }
+        // Return the sum
+        return sum;
     }
 
 }
